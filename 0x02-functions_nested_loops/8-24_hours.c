542f59a5f1326bc39413322c9b6664ffb919a336
shedrachchilaka <shedrach_chilaka@yahoo.com>
2022-10-19 15:41:27 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,10 @@
 
 void jack_bauer(void)
 {
-     int min, hr;
-     for (hr = 0; hr <= 23; ++hr)
-     {
+        int min, hr;
+
+        for (hr = 0; hr <= 23; ++hr)
+        {
 	     for (min = 0; min <= 59; ++min)
 	     {
 		     _putchar((hr / 10) + 48);
